19ed2e025b3de114352de488a2fced10bc1da04e
Alexander <som17son25@gmail.com>
2023-02-13 13:24:46 +0300

    added data in MyPortfolioData

--- src/assets/Data/MyPortfolioData/MyPortfolioData.ts
+++ src/assets/Data/MyPortfolioData/MyPortfolioData.ts
@@ -1,8 +1,10 @@
 export const MyPortfolioData: WorkType[] = [
-    {title: 'Название проекта', description: 'Описание проекта'},
-    {title: 'Название проекта', description: 'Описание проекта'},
-    {title: 'Название проекта', description: 'Описание проекта'},
-    {title: 'Название проекта', description: 'Описание проекта'},
+    {title: 'THE SOCIAL NETWORK', description: 'React, TS, Redux, Rest API, Thunk, ANT Design, Unit Tests'},
+    {title: 'TODOLIST', description: 'React, TS, Redux, Rest API, Thunk, Material UI, Unit Tests'},
+    {title: 'REACT TASKS', description: 'React, TS, Redux, Rest API, Thunk, Unit Tests'},
+    {title: 'COUNTER', description: 'React, TS, Redux'},
+    {title: 'FEATURE PROJECT', description: 'In progress...'},
+    {title: 'FEATURE PROJECT', description: 'In progress...'},
 ]
 
 export type WorkType = {
