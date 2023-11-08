056df00cff6a8d077e9f6666c1c9cb24897afc50
Alexander <som17son25@gmail.com>
2023-11-08 20:34:25 +0300

    add project

--- src/assets/Data/MyPortfolioData/MyPortfolioData.ts
+++ src/assets/Data/MyPortfolioData/MyPortfolioData.ts
@@ -3,8 +3,14 @@ import todoListIcon from '../../images/works-images/todo-list.jpg'
 import reactTasksIcon from '../../images/works-images/react-tasks.jpg'
 import counterIcon from '../../images/works-images/counter.jpg'
 import futureIcon from '../../images/works-images/future.jpg'
+import infoPortalIcon from '../../images/works-images/sosyal.png'
 
 export const myPortfolioData: WorkType[] = [
+    {
+         title: 'INFORMATION PORTAL',
+         description: 'React, Redux Toolkit, RTK Query, Typescript, Webpack, Storybook, Loki, Jest, i18next,Github actions, Husky',
+         images: infoPortalIcon
+    },
     {
         title: 'THE SOCIAL NETWORK',
         description: 'React, TS, Redux, Rest API, Thunk, ANT Design, Unit Tests',
@@ -29,12 +35,7 @@ export const myPortfolioData: WorkType[] = [
         title: 'FEATURE PROJECT',
         description: 'In progress...',
         images: futureIcon
-    },
-    {
-        title: 'FEATURE PROJECT',
-        description: 'In progress...',
-        images: futureIcon
-    },
+    }
 ]
 
 export type WorkType = {
